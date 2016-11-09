292dfd85073293421b1d6b9b857627b26b2c4a74
Tom Potter <tp10g13@soton.ac.uk>
2016-11-09 11:53:54 +0000

    Initial commit with campaign list being updated and printed

---
+++ adx-agent/compile.bat
@@ -0,0 +1,9 @@
+
+javac -classpath "lib/*" SimpleAdNetwork.java
+
+jar cvf SimpleAdNetwork.jar *.class
+
+move SimpleAdNetwork.jar lib/SimpleAdNetwork.jar
+
+
+pause
---
+++ adx-agent/runLogParser.bat
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Usage
+#   sh ./runServer.sh
+#
+
+TACAA_HOME=`pwd`
+echo $TACAA_HOME
+echo $CLASSPATH
+java -cp "lib/*" se.sics.tasim.logtool.Main -handler tau.tac.adx.parser.GeneralHandler -file "C:/Users/Tom/Documents/4thYear/IntelligentAgents/adx/logs/game.slg.gz" -ucs -rating -bank -campaign -adnet
